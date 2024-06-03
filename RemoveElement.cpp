5523f571d1f589aadfe3f0893261535b469b0084
Ayushi Verma <97012057+ayushiv2@users.noreply.github.com>
2024-06-03 21:11:56 +0530

    Create RemoveElement.cpp

---
+++ RemoveElement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int l = nums.size();
+        int k=0;
+        for (int i=0; i<l; i++){
+            if (nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
