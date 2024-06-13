0a02b16157208c0cf90e8896423aca181215ad3d
Ayushi Verma <97012057+ayushiv2@users.noreply.github.com>
2024-06-13 23:57:59 +0530

    Create PascalsTriangle.cpp

---
+++ PascalsTriangle.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> pt(numRows);
+        
+        for(int i = 0; i < numRows; i++) {
+            pt[i].resize(i + 1);
+            pt[i][0] = pt[i][i] = 1;
+            
+            for(int j = 1; j < i; j++)
+                pt[i][j] = pt[i - 1][j - 1] + pt[i - 1][j];
+        }
+        return pt;
+    }
+};
