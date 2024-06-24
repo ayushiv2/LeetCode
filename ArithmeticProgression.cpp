ca371727617d5c7b57cf4ae788a58e78194e9b98
Ayushi Verma <97012057+ayushiv2@users.noreply.github.com>
2024-06-24 17:41:02 +0530

    Update ArithmeticProgression.cpp

--- ArithmeticProgression.cpp
+++ ArithmeticProgression.cpp
@@ -3,7 +3,7 @@ public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
         
         sort(arr.begin(),arr.end());
-        int n=arr[1]-arr[0];
+        int n = arr[1]-arr[0];
         for(int i=2;i<arr.size();i++){
             if (arr[i]-arr[i-1]!= n){
                 return false;
