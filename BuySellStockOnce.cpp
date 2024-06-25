7fe87f8d921e05b4263caad5fe807638c3381f5b
Ayushi Verma <97012057+ayushiv2@users.noreply.github.com>
2024-06-25 14:49:44 +0530

    Update BuySellStockOnce.cpp

--- BuySellStockOnce.cpp
+++ BuySellStockOnce.cpp
@@ -3,7 +3,7 @@ public:
     int maxProfit(vector<int>& prices) {
         int buy = prices[0];
         int profit = 0;
-        for (int i = 1; i < prices.size(); i++) {
+        for (int i = 1; i < prices.size(); i++) { 
             if (prices[i] < buy) {
                 buy = prices[i];
             } else if (prices[i] - buy > profit) {
