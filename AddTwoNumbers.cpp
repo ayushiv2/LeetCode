50c2e975a00dcb7eeeccd3457068d3b91d893a21
Ayushi Verma <97012057+ayushiv2@users.noreply.github.com>
2024-06-30 14:55:03 +0530

    Update AddTwoNumbers.cpp

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -8,7 +8,7 @@ public:
             int x=l1? l1 ->val:0;
             int y=l2? l2 ->val:0;
             int sum=carry+x+y;
-            carry=sum/10;
+            carry=sum/10; 
             temp->next=new ListNode(sum%10);
             temp=temp->next;
             l1= l1?l1->next:nullptr;
